6c92187efdd59f5b30f0940e75fd0a54caa70573
Yurii <89320dota@gmail.com>
2019-12-06 16:22:40 +0200

    added edit method

--- level2/main.js
+++ level2/main.js
@@ -1,9 +1,21 @@
 const app = new Vue({
   el: "#app",
   data: {
+    editFriend: null,
     friends: []
   },
   methods: {
+    updateFriend(friend) {
+      fetch("http://rest.learncode.academy/api/vue-5/friends/" + friend.id, {
+        body: JSON.stringify(friend),
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then(() => {
+        this.editFriend = null;
+      });
+    },
     deleteFriend(id, i) {
       fetch("http://rest.learncode.academy/api/someuser/friends/" + id, {
         method: "DELETE"
@@ -20,10 +32,18 @@ const app = new Vue({
       });
   },
   template: `
-        <div>
-            <li v-for="friend, i in friends">
-              <button v-on:click="deleteFriend(friend.id, i)">x</button>{{friend.name}}
-            </li> 
-        </div>
+  <div>
+  <li v-for="friend, i in friends">
+    <div v-if="editFriend === friend.id">
+      <input v-on:keyup.13="updateFriend(friend)" v-model="friend.name" />
+      <button v-on:click="updateFriend(friend)">save</button>
+    </div>
+    <div v-else>
+      <button v-on:click="editFriend = friend.id">edit</button>
+      <button v-on:click="deleteFriend(friend.id, i)">x</button>
+      {{friend.name}}
+    </div>
+  </li>
+</div>
     `
 });
