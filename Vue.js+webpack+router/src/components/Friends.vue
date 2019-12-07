876ab6ca32625b1e4405eee662eff57b5e24d962
Yurii <89320dota@gmail.com>
2019-12-07 10:05:14 +0200

    added mini project with router

---
+++ Vue.js+webpack+router/src/components/Friends.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <h1>Friends</h1>
+    id: {{id}}
+    Age: {{age}} 
+    Weight: {{weight}}
+  </div>
+</template>
+
+<script>
+export default {  
+  props: [
+    'id',
+    'age',
+    'weight',
+  ]
+}
+</script>
